bc1841ef3715f384f4c1324198360d881cb835df
Mario Đurašin <indiche@gmail.com>
2014-05-06 10:32:17 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+var gulp = require('gulp'),
+    util = require('gulp-util'),
+    es = require('event-stream'),
+    http = require('http'),
+    sass = require('gulp-sass'),
+    jshint = require('gulp-jshint'),
+    connect = require('connect'),
+    tiny_lr = require('tiny-lr'),
+    connectLr = require('connect-livereload');
+
+var outputDir = 'build'
+
+var target = process.env.TARGET || 'development';
+
+gulp.task('sass', function() {
+    var config = {};
+
+    if(target === 'development')
+        config.sourceComments = 'map';
+
+    if(target === 'production')
+        config.outputStyle = 'compressed';
+
+    gulp.src('app/style/style.scss')
+        .pipe(sass(config))
+        .pipe(gulp.dest('build/css/'))
+        .pipe(reload());
+});
+
+gulp.task('lint', function() {
+    gulp.src('app/js/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('app/js/**/*.js', ['lint']);
+    gulp.watch('app/style/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['lint', 'sass', 'connect', 'watch']);
+
+
+gulp.task('connect', function() {
+    startServer();
+});
+
+var lr;
+
+var startServer = function() {
+    var appPort = 3000,
+        livereloadPort = 35729,
+        server,
+        app;
+
+    var middleware = [];
+
+    middleware.push(connectLr({ port: livereloadPort }));
+    middleware.push(connect.static(outputDir));
+
+    app = connect.apply(null, middleware);
+    server = http.createServer(app);
+
+    app.use(connect.directory(outputDir));
+
+    server.listen(appPort);
+
+    util.log(util.colors.green('Server started http://localhost:' + appPort));
+
+    tiny_lr.Server.prototype.error = function() {};
+    lr = tiny_lr();
+    lr.listen(livereloadPort);
+
+    util.log(util.colors.green('LiveReload started on port: ' + livereloadPort));
+}
+
+var reload = function() {
+    return es.map(function(file, callback) {
+        lr.changed({
+            body: {
+                files: file.path
+            }
+        });
+
+        return callback(null, file);
+    });
+}
